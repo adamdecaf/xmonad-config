c44dcc2f7b671e7696889732ce74cb7ba85dce25
Adam Shannon <adam@ashannon.us>
2012-08-05 14:32:28 -0500

    First

---
+++ xmonad.hs
@@ -0,0 +1,28 @@
+-- My xmonad setup
+import XMonad
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+import XMonad.Actions.CycleWS
+import XMonad.Actions.SpawnOn
+import XMonad.Hooks.SetWMName
+import XMonad.Config.Gnome
+import Data.Monoid
+
+main = xmonad $ gnomeConfig
+  { borderWidth = 1,
+    normalBorderColor = "#000000",
+    focusedBorderColor = "#FFFFFF"
+  } `additionalKeysP` extraKeys
+
+internet = "chromium-browser"
+emacs = "emacs"
+goToSleep = ""
+lockScreen = "gnome-screensaver-command -l"
+
+extraKeys =
+  [ ("M-i", spawn internet)
+  , ("M-e", spawn emacs)
+  , ("M-s", spawn goToSleep)
+  , ("M-l", spawn lockScreen)
+  ]
+
