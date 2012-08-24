822ed6857321c0ed27a9037ca58177f5beb37a5a
Adam Shannon <adam@ashannon.us>
2012-08-23 19:22:26 -0500

    Updates.

--- xmonad.hs
+++ xmonad.hs
@@ -8,23 +8,27 @@ import XMonad.Hooks.SetWMName
 import XMonad.Config.Gnome
 import Data.Monoid
 
-main = xmonad $ gnomeConfig
+main = xmonad $ defaultConfig
   { borderWidth = 1,
     normalBorderColor = "#000000",
     focusedBorderColor = "#FFFFFF"
   } `additionalKeysP` extraKeys
 
 internet = "chromium-browser"
+secondaryBrowser = "firefox"
 emacs = "emacs"
 goToSleep = ""
 lockScreen = "gnome-screensaver-command -l"
 music = "spotify"
+keepass = "keepassx"
 
 extraKeys =
   [ ("M-i", spawn internet)
+  , ("M-f", spawn secondaryBrowser)
   , ("M-e", spawn emacs)
   , ("M-s", spawn goToSleep)
   , ("M-l", spawn lockScreen)
   , ("M-m", spawn music)
+  , ("M-k", spawn keepass)
   ]
 
