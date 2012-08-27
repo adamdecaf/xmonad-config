c4c69ed2cca26484bbe442f3039df29d45d4b5df
jwinder <joseph.winder@t8webware.com>
2012-08-26 19:18:13 -0500

    this one's better

--- xmonad.hs
+++ xmonad.hs
@@ -1,34 +1,74 @@
--- My xmonad setup
 import XMonad
-import XMonad.Util.Run
 import XMonad.Util.EZConfig
 import XMonad.Actions.CycleWS
 import XMonad.Actions.SpawnOn
 import XMonad.Hooks.SetWMName
-import XMonad.Config.Gnome
 import Data.Monoid
 
 main = xmonad $ defaultConfig
-  { borderWidth = 1,
-    normalBorderColor = "#000000",
-    focusedBorderColor = "#FFFFFF"
-  } `additionalKeysP` extraKeys
+       { terminal           = myTerminal
+       , workspaces         = myWorkspaces
+       , focusedBorderColor = blue
+       , normalBorderColor  = black
+       , borderWidth        = 2
+       , modMask            = mod4Mask -- masks left-alt to super for xmonad bindings
+       , manageHook         = manageSpawn <+> manageHook defaultConfig
+       , startupHook        = myStartupHook
+       } `removeKeysP` myRemoveKeysP
+         `additionalKeysP` myAdditionalKeysP
 
-internet = "chromium-browser"
-secondaryBrowser = "firefox"
-emacs = "emacs"
-goToSleep = ""
-lockScreen = "gnome-screensaver-command -l"
-music = "spotify"
-keepass = "keepassx"
+myWorkspaces = [one, two, three, four, five, six, seven, eight, nine]
 
-extraKeys =
-  [ ("M-i", spawn internet)
-  , ("M-f", spawn secondaryBrowser)
-  , ("M-e", spawn emacs)
-  , ("M-s", spawn goToSleep)
-  , ("M-l", spawn lockScreen)
-  , ("M-m", spawn music)
-  , ("M-k", spawn keepass)
-  ]
+myAdditionalKeysP =
+    [ ("M-p", spawnHere myMenu)
+    , ("M-S-w", spawnHere shutdownSystem)
+    , ("M-C-r", spawnHere myTerminal)
+    , ("M-C-e", spawnHere myEditor)
+    , ("M-C-t", spawnHere myEditorInit)
+    , ("M-C-g", spawnHere myInternet)
+    , ("M-i", nextWS)
+    , ("M-u", prevWS)
+    , ("M-S-i", shiftToNext)
+    , ("M-S-u", shiftToPrev)
+    ]
 
+myRemoveKeysP = ["M-q"]
+
+myStartupHook = setWMName "LG3D"
+                >> spawnHere "gnome-settings-daemon"
+                >> spawnHere "nm-applet"
+                >> spawnHere "feh --bg-scale $HOME/.xmonad/background.png"
+                >> spawnHere "xcompmgr"
+                >> spawnHere "python $HOME/bin/dropbox.py start"
+                >> spawnHere "$HOME/.xmonad/keymappings.sh"
+                >> spawnHere "sleep 15; $HOME/.xmonad/brightness.sh"
+                >> spawnOn nine nvidiaMenu
+                >> spawnOn four myMusic
+                >> spawnOn three myTerminal
+--                >> spawnOn two myEditorInit
+                >> spawnOn one myInternet
+
+myMenu         = "dmenu_run"
+shutdownSystem = "dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop"
+myTerminal     = "gnome-terminal"
+myEditorInit   = "emacs"
+myEditor       = "emacsclient -c"
+myInternet     = "google-chrome"
+myIm           = "pidgin"
+myMusic        = "audacious"
+
+nvidiaMenu     = "nvidia-settings"
+gnomeControl   = "gnome-control-center"
+
+blue  = "#0000FF"
+black = "#000000"
+
+one   = "1"
+two   = "2"
+three = "3"
+four  = "4"
+five  = "5"
+six   = "6"
+seven = "7"
+eight = "8"
+nine  = "9"
